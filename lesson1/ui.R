e2363d3f204b3d72f2d7d30fe784835b17a368e0
Hojoon Ji <sleeperbus@gmail.com>
2015-06-19 17:05:23 +0900

    ...

--- lesson1/ui.R
+++ lesson1/ui.R
@@ -5,15 +5,14 @@ shinyUI(fluidPage(
 
 	sidebarLayout(
 		sidebarPanel(
-			"bins",
-			"Number of bins:",
-			min = 1,
-			max = 50,
-			value = 30)
-	),	
-
-	mainPanel(
-		plotOutput("distPlot")
-	)
-
+            sliderInput(
+                "bins",
+                "Number of bins:",
+                min = 1,
+                max = 50,
+                value = 30
+            ) 
+        ),
+    	mainPanel(plotOutput("distPlot"))
+	)  
 ))
