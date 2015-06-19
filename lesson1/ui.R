f3162f6036714fb7d58b6462c7f29d11e8453ce5
sleeperbus <sleeperbus@gmail.com>
2015-06-19 06:12:29 +0000

    ...

--- lesson1/ui.R
+++ lesson1/ui.R
@@ -1,1 +1,19 @@
 library(shiny)
+
+shinyUI(fluidPage(
+	titlePanel("Hello Shiny!"),
+
+	sidebarLayout(
+		sidebarPanel(
+			"bins",
+			"Number of bins:",
+			min = 1,
+			max = 50,
+			value = 30)
+	),	
+
+	mainPanel(
+		plotOutput("distPlot")
+	)
+
+))
