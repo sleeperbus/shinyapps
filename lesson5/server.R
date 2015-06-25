0397826f3a334bfeeda397de626b49cdf1ad477f
sleeperbus <sleeperbus@gmail.com>
2015-06-25 07:52:10 +0000

    ..

--- lesson5/server.R
+++ lesson5/server.R
@@ -7,23 +7,15 @@ shinyServer(
   function(input, output) {
     
     output$map <- renderPlot({ 
-      data = switch(input$var,
-                    "Percent White" = counties$white,
-                    "Percent Black" = counties$black,
-                    "Percent Hispanic" = counties$hispanic,
-                    "Percent Asian" = counties$asian)
-      color = switch(input$var,
-                    "Percent White" = "darkgreen",
-                    "Percent Black" = "black",
-                    "Percent Hispanic" = "darkorange",
-                    "Percent Asian" = "darkviolet")
-      legend = switch(input$var,
-                    "Percent White" = "% White",
-                    "Percent Black" = "% Black",
-                    "Percent Hispanic" = "% Hispanic",
-                    "Percent Asian" = "% Asian")
-      percent_map(data, color, legend, input$range[1],input$range[2])
-    })
-    
+      
+      args = switch(input$var,
+                    "Percent White" = list(counties$white, "darkgreen", "% White"),
+                    "Percent Black" = list(counties$black, "black", "% Black"),
+                    "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
+                    "Percent Asian" = list(counties$asian, "darkviolet", "% Asian"))  
+      args$min = input$range[1]
+      args$max = input$range[2]
+      do.call(percent_map, args) 
+    }) 
   }
 )
