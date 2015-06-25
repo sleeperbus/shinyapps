3406d435fa878e1edd92e1e90ba96e3c641ddc37
sleeperbus <sleeperbus@gmail.com>
2015-06-25 07:41:13 +0000

    ..

---
+++ lesson3/ui.R
@@ -0,0 +1,22 @@
+shinyUI(
+  fluidPage(
+    titlePanel("censusVis"),
+    sidebarLayout(
+      sidebarPanel(
+        helpText("Create demographic maps with information ",
+                 "from the 2010 US Census."),
+        selectInput(
+          "select", label="Choose a variable to display",
+          choices=list("Percent White"=1, "Percent Black"=2,
+                       "Percent Hispanic"=3, "Percent Asian"=4),
+          selected=1
+        ),
+        sliderInput(
+          "slder", label="Range of interest", min=0, max=100,
+          value=c(0,100)
+        )
+      ),
+      mainPanel() 
+    )
+  )
+)
---
+++ lesson5/server.R
@@ -0,0 +1,29 @@
+source("helpers.R")
+counties <- readRDS("data/counties.rds")
+library(maps)
+library(mapproj)
+
+shinyServer(
+  function(input, output) {
+    
+    output$map <- renderPlot({ 
+      data = switch(input$var,
+                    "Percent White" = counties$white,
+                    "Percent Black" = counties$black,
+                    "Percent Hispanic" = counties$hispanic,
+                    "Percent Asian" = counties$asian)
+      color = switch(input$var,
+                    "Percent White" = "darkgreen",
+                    "Percent Black" = "black",
+                    "Percent Hispanic" = "darkorange",
+                    "Percent Asian" = "darkviolet")
+      legend = switch(input$var,
+                    "Percent White" = "% White",
+                    "Percent Black" = "% Black",
+                    "Percent Hispanic" = "% Hispanic",
+                    "Percent Asian" = "% Asian")
+      percent_map(data, color, legend, input$range[1],input$range[2])
+    })
+    
+  }
+)
