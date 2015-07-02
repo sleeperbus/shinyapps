52d3b22d28a6b751837fe9a42714ad2ad62d03a1
Hojoon Ji <sleeperbus@gmail.com>
2015-07-02 17:41:58 +0900

    ..

--- house_price/server.R
+++ house_price/server.R
@@ -1,26 +1,23 @@
 library(shiny)
 source("helpers.R")
 
-
-
-shinyServer(function(input, output){
-  apts = data.frame() 
-  aptNames = list()
-  
+shinyServer(function(input, output){ 
   data = reactive({
     f_makeData(input$dongCode, input$period[1], input$period[2])    
   })
-    
+
   output$ui = renderUI({
+    apts = data()
+    aptNames = list()
     uniqueApts = apts[, c("APT_NAME", "APT_CODE")]
-    uniqueApts = uniqueApts[,!duplicated(uniqueApts)]
+    uniqueApts = uniqueApts[!duplicated(uniqueApts),]
     aptNames = as.list(uniqueApts[,2])
     names(aptNames) = uniqueApts[,1]
     checkboxGroupInput("aptCodes", "Select APTs", choices=aptNames)
-  })
-    
+  })    
+  
   output$plot = renderPlot({
     apts = data() 
-    f_plot(apts)    
-  }) 
+    f_plot(data=apts, aptCodes=input$aptCodes, pyungs=input$pyung)    
+  })   
 })
--- house_price/ui.R
+++ house_price/ui.R
@@ -5,7 +5,7 @@ shinyUI(fluidPage(
     sidebarPanel(
       textInput("dongCode", "DongCode:", "2826010300"),
       sliderInput("period", "Period:", min=2006, max=2015, value=c(2013, 2015)), 
-      checkboxGroupInput("pyung", "Pyung", list("24-"=1, "28"=2, "33"=3, "40+"=4)),
+      checkboxGroupInput("pyung", "Pyung", list("24-"=24, "28"=28, "33"=33, "40+"=40)),
       uiOutput("ui") 
     ),
     mainPanel(plotOutput("plot"))
