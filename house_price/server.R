91e4965beac09d8436b50c6355f65e7529efff4a
Ubuntu <ubuntu@ip-172-31-30-235.ap-northeast-1.compute.internal>
2015-07-02 07:18:52 +0000

    ...

--- house_price/server.R
+++ house_price/server.R
@@ -1,18 +1,26 @@
 library(shiny)
 source("helpers.R")
 
-apts = data.frame() 
+
 
 shinyServer(function(input, output){
-    data = reactive({
-        f_makeData(input$dongCode, input$period[1], input$period[2])    
-    })
+  apts = data.frame() 
+  aptNames = list()
+  
+  data = reactive({
+    f_makeData(input$dongCode, input$period[1], input$period[2])    
+  })
     
-    output$ui = renderUI({
-    })
+  output$ui = renderUI({
+    uniqueApts = apts[, c("APT_NAME", "APT_CODE")]
+    uniqueApts = uniqueApts[,!duplicated(uniqueApts)]
+    aptNames = as.list(uniqueApts[,2])
+    names(aptNames) = uniqueApts[,1]
+    checkboxGroupInput("aptCodes", "Select APTs", choices=aptNames)
+  })
     
-    output$plot = renderPlot({
-        apts = data() 
-        f_plot(apts)    
-    }) 
+  output$plot = renderPlot({
+    apts = data() 
+    f_plot(apts)    
+  }) 
 })
