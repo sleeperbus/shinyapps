6137261870b5cff2a67a8e1461b35927825dd153
Ubuntu <ubuntu@ip-172-31-30-235.ap-northeast-1.compute.internal>
2015-07-15 02:20:43 +0000

    .

--- house_price/server.R
+++ house_price/server.R
@@ -12,7 +12,6 @@ shinyServer(function(input, output){
   data = reactive({
     print("get data active")
     data = f_makeData("2826010300", minYear, maxYear)
-#     data = f_makeData(input$dongCode, minYear, maxYear)
     return(data)
   })
    
@@ -25,7 +24,7 @@ shinyServer(function(input, output){
     selectInput("sido", "Sido", choices=codes, selected=sido[1,1])
   })
   
-  output$ui = renderUI({
+  output$aptNames = renderUI({
     print("dynamic ui active")
     apts = data()
     aptNames = list()
@@ -43,8 +42,8 @@ shinyServer(function(input, output){
     aptCodes = input$aptCodes
     pyungs = input$pyung
     
-    result = subset(apts, SALE_YEAR >= input$period[1] & SALE_YEAR <= input$period[2])   
-    result = subset(result, APT_CODE %in% aptCodes)
+    result = subset(apts, SALE_YEAR >= input$period[1] & SALE_YEAR <= input$period[2])
+		result = subset(result, APT_CODE %in% aptCodes)
     result = subset(result, PYUNG %in% pyungs)
     result$APT_NAME = factor(result$APT_NAME, ordered=F)
     result$PYUNG = factor(result$PYUNG)
@@ -52,14 +51,11 @@ shinyServer(function(input, output){
     
     ggvis(result, x=~SALE_DATE, y=~SUM_AMT) %>%
       group_by(GROUP) %>%
-#       group_by(APT_NAME, PYUNG) %>%
-#       layer_points(fill=~APT_NAME, opacity:=0.4) %>%
       layer_points(fill=~GROUP, opacity:=0.4) %>%
       layer_smooths(stroke= ~GROUP) %>%
       add_tooltip(function(df) df$SUM_AMT) %>%
       add_axis("x", title="매매시점") %>% 
       add_axis("y", title="매매가격", title_offset=70) 
-#       add_legend(scales=c("fill"), title="APT NAME")
   }) 
 
   vis %>% bind_shiny("plot")  
--- house_price/ui.R
+++ house_price/ui.R
@@ -2,18 +2,27 @@ library(shiny)
 library(ggvis)
 shinyUI(fluidPage(
   titlePanel("House Prices Tracker"),
-  sidebarLayout(
-    sidebarPanel(
-      uiOutput("sido"),
-      sliderInput("period", "Period:", min=2006, max=2015, value=c(2010, 2015)), 
+	ggvisOutput("plot"),
+	hr(),
+	fluidRow(
+		column(3, 
+			h4("Region"),
+			uiOutput("sido"),
+			br(),
+			uiOutput("gugun"),
+			br(),
+			uiOutput("dong")
+		), 
+		column(3,
+      sliderInput("period", "Period:", min=2006, max=2015, value=c(2010, 2015))
       checkboxGroupInput("pyung", 
                          "Pyung", 
                          list("24-"=24, "28"=28, "33"=33, "40+"=40),
-                         selected=33),
-      uiOutput("ui")
-    ),
-    mainPanel(
-      ggvisOutput("plot")
-    )
-  )
+                         selected=33)
+		),
+		column(3, 
+			h4("Select APTs"),
+			uiOutput("aptNames")
+		)
+	) 
 ))
