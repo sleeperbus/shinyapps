f82f2e104e47ed5d90186e5daa4697354eb26eb3
sleeperbus <sleeperbus@gmail.com>
2015-07-24 06:37:52 +0900

    ..

--- house_price/data_crawler.R
+++ house_price/data_crawler.R
@@ -4,7 +4,7 @@ dong = readRDS("data/dong.rds")
 seoulDong = subset(dong, sidoCode == "11")
 seoulSample = seoulDong[sample(10), ]
 
-result = apply(as.data.frame(dong[,3]), 1, f_makeData, 2014, 2014)
+result = apply(as.data.frame(dong[,3]), 1, f_makeData, 2006, 2014)
 #result = apply(as.data.frame(seoulSample[,3]), 1, f_makeData, 2015, 2015)
 #result = apply(as.data.frame(dong[,3]), 1, f_makeData, 2015, 2015)
 result = do.call("rbind", result)
